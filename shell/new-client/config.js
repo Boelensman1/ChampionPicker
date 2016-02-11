134aef27cf457f499edab015d05c4bd1e474194c
Boelensman1 <me@wiggerboelens.com>
2016-02-11 19:19:46 +0100

    Moved everything around; general cleanup
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

---
+++ shell/new-client/config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    dbModelLocation: 'shell/new-client/models',
+    dbConnectionConfig: {
+        name: 'random2',
+        port: '5432',
+        user: 'random2',
+        options: {
+            dialect: 'postgres',
+            host: '127.0.0.1',
+            logging: false
+        }
+    }
+};
